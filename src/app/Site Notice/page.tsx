2a5959fd4a19e1b11bfb70ecde34f8290bb1462b
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-09-21 15:29:28 +0200

    added Site Notice and Privacy Policy

---
+++ src/app/Site Notice/page.tsx
@@ -0,0 +1,30 @@
+export default function SiteNotice() {
+  return (
+    <main>
+      <h1>Impressum</h1>
+      <br />
+
+      <p>Angaben gemäß § 5 TMG: Stuckwerkstatt Voitenko </p>
+      <p>Inhaber: Oleksandr Voitenko</p>
+      <p>Adresse: Berliner Str 23A, 19300, Grabow </p>
+      <br />
+      <p>Kontakt</p>
+      <p>Telefon: +491639252077</p>
+      <p>E-Mail: stuckwerkstattvoitenko@gmail.com</p>
+      <br />
+
+      <p> Umsatzsteuer-ID: [DE...]</p>
+      {/* (сюда немецкий налоговый номер, если есть; если
+      нет — убери эту строку)  */}
+      <p> Handelsregister:</p>
+      {/* [Registergericht, HRB-Nummer] */}
+      {/* (сюда данные из торгового реестра, если есть; если у тебя
+      Einzelunternehmen без Handelsregister — убери эту строку) */}
+      <p>
+        Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: Oleksandr
+        Voitenko,Berliner Str 23A, 19300, Grabow{" "}
+      </p>
+      {/* [Name, Anschrift] (сюда снова твое имя и адрес, как выше) */}
+    </main>
+  );
+}
